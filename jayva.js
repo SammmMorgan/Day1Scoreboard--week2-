73a8867044c1c919a3b4edfec2fe30521ceed9a6
Sam Morgan <sam2morgan99@gmail.com>
2024-01-29 16:49:22 -0700

    functional but ugly

--- jayva.js
+++ jayva.js
@@ -12,19 +12,33 @@ let scoreNotGamers = 0
 function plusOnebutton() {
     scoreGamers += 1
     console.log(`Score: ${scoreGamers}`);
+    activeScore()
 }
 
 function plusTwoButton() {
     scoreGamers += 2
     console.log(`Score: ${scoreGamers}`)
+    activeScore()
 }
 
 function plusOnebuttonTwo() {
     scoreNotGamers += 1
     console.log(`Score: ${scoreNotGamers}`);
+    activeNotScore()
 }
 
 function plusTwoButtonTwo() {
     scoreNotGamers += 2
     console.log(`Score: ${scoreNotGamers}`)
+    activeNotScore()
+}
+
+function activeScore() {
+    const currentScoreGamer = document.getElementById('scoreGamer');
+
+    currentScoreGamer.innerText = scoreGamers
+}
+function activeNotScore() {
+    const currentScoreNotGamer = document.getElementById('scoreNotGamer');
+    currentScoreNotGamer.innerText = scoreNotGamers
 }
