065ec2d123f5063bdfce4a952f511fef05b91d4a
Sam Morgan <sam2morgan99@gmail.com>
2024-01-29 16:10:33 -0700

    Buttons Function (kinda)

---
+++ jayva.js
@@ -0,0 +1,30 @@
+// console.log('shit works');
+
+function buttonClick() {
+    const message = 'Button press'
+    console.log(message);
+}
+
+
+let scoreGamers = 0
+let scoreNotGamers = 0
+
+function plusOnebutton() {
+    scoreGamers += 1
+    console.log(`Score: ${scoreGamers}`);
+}
+
+function plusTwoButton() {
+    scoreGamers += 2
+    console.log(`Score: ${scoreGamers}`)
+}
+
+function plusOnebuttonTwo() {
+    scoreNotGamers += 1
+    console.log(`Score: ${scoreNotGamers}`);
+}
+
+function plusTwoButtonTwo() {
+    scoreNotGamers += 2
+    console.log(`Score: ${scoreNotGamers}`)
+}
